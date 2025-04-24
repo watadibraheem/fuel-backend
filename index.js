23e6f49b3850683ffc3be604f2a78dbf584f53a1
Ibraheem Watad <ibraheemwatad35@gmail.com>
2025-04-24 16:46:08 +0300

    Fixed

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
